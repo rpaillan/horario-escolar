b6a52b3c77f65dc9c4c13fa70e24cfc3145dd809
Paillan <rodrigo.paillan@gmail.com>
2024-08-28 00:29:25 -0400

    Added axios example

--- src/Horario.tsx
+++ src/Horario.tsx
@@ -1,7 +1,8 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useEffect } from "react";
 import { data, lecturesMap } from "./data";
 
 import "./Horario.scss";
+import axios from "axios";
 
 function formatTime(value: number): string {
   let time = String(value);
@@ -18,54 +19,71 @@ interface MyCustomCSS extends CSSProperties {
 }
 
 export const HorarioContainer = () => {
-  return (
-    <div className="horario">
-      {data.days.map((day) => (
-        <div className="cell day">{day.label}</div>
-      ))}
-      {data.modulos.map((sector) => {
-        if (sector.isBreak) return;
-        return (
-          <>
-            {data.days.map((day) => {
-              const key = day.name + "-" + sector.label;
-              if (lecturesMap[key]) {
-                const { lecture, classe, sectors, enabled } = lecturesMap[key];
-                if (!enabled) return null;
-                console.log({ lecture, classe, sectors });
+  useEffect(() => {
+    axios
+      .get(
+        "https://cataas.com/cat?type=square&position=center&width=300&height=300",
+        {
+          headers: {
+            Accept: "image/*",
+          },
+        }
+      )
+      .then((data) => console.log(data.data));
+  }, []);
 
-                const startTime = formatTime(sectors[0].times.start);
-                const endTime = formatTime(
-                  sectors[sectors.length - 1].times.end
-                );
-                return (
-                  <div
-                    className={"cell lecture span-" + classe.modulos.length}
-                    style={
-                      {
-                        gridRow: "span " + classe.modulos.length,
-                        "--sidebar-color": lecture.color || "white",
-                      } as MyCustomCSS
-                    }
-                  >
-                    <div className="sidebar"></div>
-                    <div className="body">
-                      <div className="lecture-name">{lecture.name}</div>
-                      <div className="lecture-teacher">
-                        ({lecture.teacher})
-                        <span className="time">{startTime}</span>{" "}
-                        <span className="time">{endTime}</span>
+  return (
+    <>
+      <img src="https://cataas.com/cat?type=square&position=center&width=300&height=300" />
+      <div className="horario">
+        {data.days.map((day) => (
+          <div className="cell day">{day.label}</div>
+        ))}
+        {data.modulos.map((sector) => {
+          if (sector.isBreak) return;
+          return (
+            <>
+              {data.days.map((day) => {
+                const key = day.name + "-" + sector.label;
+                if (lecturesMap[key]) {
+                  const { lecture, classe, sectors, enabled } =
+                    lecturesMap[key];
+                  if (!enabled) return null;
+                  console.log({ lecture, classe, sectors });
+
+                  const startTime = formatTime(sectors[0].times.start);
+                  const endTime = formatTime(
+                    sectors[sectors.length - 1].times.end
+                  );
+                  return (
+                    <div
+                      className={"cell lecture span-" + classe.modulos.length}
+                      style={
+                        {
+                          gridRow: "span " + classe.modulos.length,
+                          "--sidebar-color": lecture.color || "white",
+                        } as MyCustomCSS
+                      }
+                    >
+                      <div className="sidebar"></div>
+                      <div className="body">
+                        <div className="lecture-name">{lecture.name}</div>
+                        <div className="lecture-teacher">
+                          ({lecture.teacher})
+                          <span className="time">{startTime}</span>{" "}
+                          <span className="time">{endTime}</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                );
-              } else {
-                return <div className="cell free"></div>;
-              }
-            })}
-          </>
-        );
-      })}
-    </div>
+                  );
+                } else {
+                  return <div className="cell free"></div>;
+                }
+              })}
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 };
